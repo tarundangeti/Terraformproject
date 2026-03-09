35635f09724aebaa161cdb148a22990af7329fa2
Tarun D <tarub.dangeti2@gmail.com>
2026-03-09 21:46:06 +0000

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ins_name = "TFSERVER"
+ins_type = "t2.micro"
+ins_key = "tarunpem5/02/2025"
+ins_ami = "ami-06c643a49c853da56"
--- variable.tf
+++ variable.tf
@@ -1,26 +1,22 @@
 variable "ins_name" {
   description = "This is instance Name"
   type        = string
-  default     = "TF-SERVER-AUTO"
 
 }
 
 variable "ins_type" {
   description = "this is default instance type for free tier"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "ins_ami" {
   description = "this is default ami for linux server"
   type        = string
-  default     = "ami-022d03f649d12a49d"
 
 }
 
 variable "ins_key" {
   description = "this default key pair for this server"
   type        = string
-  default     = "tarunpem5/02/2025"
 
 }
